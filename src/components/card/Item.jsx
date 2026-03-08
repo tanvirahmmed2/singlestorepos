2ecce5378718bd0ef6f64fafe97dfa8dbaece90b
Tanvir Ahmmed <tanvir004006@gmail.com>
2026-03-08 17:58:15 +0600

    item

--- src/components/card/Item.jsx
+++ src/components/card/Item.jsx
@@ -10,19 +10,23 @@ const Item = ({ product }) => {
   const { addToCart } = useContext(Context)
 
   return (
-    <motion.div initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ duration: 0.6 }} className='w-full relative flex flex-col items-center gap-1 p-2 transition ease-in-out duration-500 shadow hover:shadow-lg justify-between cursor-pointer bg-white group overflow-hidden '>
-     <div className='w-full aspect-square overflow-hidden '>
-       <Image src={`${product?.image}`} alt='image' width={1000} height={1000} className='w-full aspect-square object-cover group-hover:scale-105 transition ease-in-out duration-500' />
-     </div>
-      <Link href={`/products/${product?.slug}`}>{product?.name}</Link>
-      {
-        product?.discount_price > 0 ? <div>
-          <p className='text-red-400 line-through'>BDT {product.sale_price}</p>
-          <p>BDT {product?.sale_price - product?.discount_price}</p>
-        </div> : <p>BDT {product.sale_price}</p>
-      }
-      <button onClick={() => addToCart(product)} className='w-full p-1 sm:absolute bottom-0 z-10 group-hover:translate-y-0 translate-y-0 sm:translate-y-full transition ease-in-out duration-500 bg-orange-500 text-white cursor-pointer hover:bg-orange-700'>Add to cart</button>
+    <motion.div initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ duration: 0.7 }} className='w-full border flex flex-col items-center justify-between cursor-pointer border-black/10 hover:shadow-lg shadow group overflow-hidden transition ease-in-out duration-500 relative'>
+      <Link href={`/products/${product.slug}`} className='w-full flex items-center gap-1 flex-col p-2  relative '>
+        <div className='w-full overflow-hidden'>
+          <Image src={`${product?.image}`} alt='image' width={1000} height={1000} className='w-full group-hover:scale-105 transition ease-in-out duration-500 aspect-square object-cover overflow-hidden '  />
+        </div>
+        <p className='text-xs w-full text-center md:text-sm'>{product?.name}</p>
+        {
+          product?.discount_price > 0 ? <div>
+            <p className='text-red-400 line-through'>BDT {product.sale_price}</p>
+            <p>BDT {product?.sale_price - product?.discount_price}</p>
+          </div> : <p>BDT {product.sale_price}</p>
+        }
+      </Link>
+      <button onClick={() => addToCart(product)} className='w-full sm:absolute z-10 bottom-0 sm:p-1 transition ease-in-out duration-500 bg-orange-400 text-white transform cursor-pointer group-hover:translate-y-0 translate-y-0 sm:translate-y-full'>Add to cart</button>
     </motion.div>
+
+    
   )
 }
 
