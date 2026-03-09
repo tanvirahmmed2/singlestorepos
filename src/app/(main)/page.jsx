ee7a088352549b6507eed786cc2b706dbb1b1131
Tanvir Ahmmed <tanvir004006@gmail.com>
2026-03-09 21:45:42 +0600

    clear

--- src/app/(main)/page.jsx
+++ src/app/(main)/page.jsx
@@ -1,17 +1,9 @@
-import Footer from '@/components/bar/Footer'
-import Intro from '@/components/page/Intro'
-import Offer from '@/components/page/Products'
-import StoreVisit from '@/components/page/StoreVisit'
-import Support from '@/components/page/Support'
+
 import React from 'react'
 
 const MainPage = () => {
   return (
     <div className='w-full min-h-screen flex flex-col'>
-      <Intro />
-      <Offer/>
-      <StoreVisit/>
-      <Support />
     </div>
   )
 }
