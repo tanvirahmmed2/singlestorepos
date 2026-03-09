b17084c6a9faef4fa35b7124211bc5a18b8efe80
Tanvir Ahmmed <tanvir004006@gmail.com>
2026-03-10 01:52:04 +0600

    base

--- src/lib/database/secret.js
+++ src/lib/database/secret.js
@@ -1,5 +1,5 @@
 
-export const BASE_URL = 'https://www.nizamvarietiesstore.com';
+export const BASE_URL = 'https://singlestorepos.disibin.com';
 
 export const CLOUDINARY_NAME = process.env.CLOUDINARY_NAME;
 export const CLOUDINARY_API = process.env.CLOUDINARY_API;
