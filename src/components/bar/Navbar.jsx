39fe74758ee57ba01ab3d11d6866d00bbced6456
Tanvir Ahmmed <tanvir004006@gmail.com>
2026-03-09 23:28:50 +0600

    updated

--- src/components/bar/Navbar.jsx
+++ src/components/bar/Navbar.jsx
@@ -5,7 +5,7 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div className='w-full relative'>
-      <nav className='w-full flex flex-row items-center justify-between fixed top-0 right-0 h-14 px-4 shadow z-50'>
+      <nav className='w-full flex text-xs sm:text-base flex-row bg-white items-center justify-between fixed top-0 right-0 h-14 px-4 shadow z-50'>
         <Link href={'/'} className='text-lg sm:text-2xl font-semibold'>Store Management</Link>
         <div className='w-auto flex flex-row items-center justify-center gap-4'>
           <Link href={'https://api.whatsapp.com/send/?phone=8801805003886&text&type=phone_number&app_absent=0'}>Discuss</Link>
